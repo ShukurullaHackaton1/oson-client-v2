6c8639619292e24c42f35e15768cf962a54cbb79
ShukurullaHackaton1 <sayidabdullatursinbayev205@gmail.com>
2025-09-03 14:00:53 +0500

    start

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5009",
+        target: "http://oson.kerek.uz/",
         changeOrigin: true,
       },
     },
