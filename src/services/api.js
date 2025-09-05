130f43e98369325db6bfe70a7202b1e22c68e7b5
ShukurullaHackaton1 <sayidabdullatursinbayev205@gmail.com>
2025-09-05 15:12:56 +0500

    .

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "http://localhost:5009/api",
+  baseURL: "https://oson.kerek.uz/api",
   timeout: 10000,
 });
 
